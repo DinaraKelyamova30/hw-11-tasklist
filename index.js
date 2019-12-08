6e09ffadd08ccab2063071a11ae74d11eb72ed0a
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-12-08 20:51:14 +0200

    js

--- index.js
+++ index.js
@@ -3,25 +3,33 @@ const addText = document.querySelector('.addText');
 const checking = document.querySelector('.check');
 const n = document.querySelectorAll('.n');
 const hide = document.querySelectorAll('.hide');
+const tasks = document.querySelector('.tasks');
 
-function addTask() {
-    let newDiv = document.createElement('div');
-    newDiv.className = 'hide';
-    let newCheck = document.createElement('input');
-    newCheck.type = 'checkbox';
-    newCheck.id = addText.value;
-    newCheck.class = 'n';
-    let newLabel = document.createElement('label');
-    newLabel.for = addText.value;
-    newLabel.textContent = addText.value;
-    newDiv.appendChild(newCheck);
-    newDiv.appendChild(newLabel);
-    checking.appendChild(newDiv);
+function addTaskBtn() {
+    if (!addText.value) {
+        alert('Пусте поле дадавати не можна');
+    } else {
+        let newDiv = document.createElement('div');
+        newDiv.className = 'hide';
+        let newCheck = document.createElement('input');
+        newCheck.type = 'checkbox';
+        newCheck.id = addText.value;
+        newCheck.class = 'n';
+        let newLabel = document.createElement('label');
+        newLabel.for = addText.value;
+        newLabel.textContent = addText.value;
+        newDiv.appendChild(newCheck);
+        newDiv.appendChild(newLabel);
+        checking.appendChild(newDiv);
+        addText.value = '';
+    }
 }
 
-
-    n.onclick = function(){
-        for(let n = 0; hide.length; i++){
-            hide.style.display = 'none';
+function removeTask(arr) {
+    for (let i = 0; i < hide.length; i++) {
+        if(hide[i].parentNode){
+            hide[i].parentNode.removeChild(hide[i]);
         }
     }
+}
+
