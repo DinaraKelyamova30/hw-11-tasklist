e380211a990fafc57a21ffb2d3c726eb1fa945a6
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-12-09 15:53:52 +0200

    js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const n = document.querySelectorAll('.n');
 const hide = document.querySelectorAll('.hide');
 const tasks = document.querySelector('.tasks');
 
+
 function addTaskBtn() {
     if (!addText.value) {
         alert('Пусте поле дадавати не можна');
@@ -12,9 +13,9 @@ function addTaskBtn() {
         let newDiv = document.createElement('div');
         newDiv.className = 'hide';
         let newCheck = document.createElement('input');
+        newCheck.classList.add('n');
         newCheck.type = 'checkbox';
         newCheck.id = addText.value;
-        newCheck.class = 'n';
         let newLabel = document.createElement('label');
         newLabel.for = addText.value;
         newLabel.textContent = addText.value;
@@ -25,11 +26,10 @@ function addTaskBtn() {
     }
 }
 
-function removeTask(arr) {
-    for (let i = 0; i < hide.length; i++) {
-        if(hide[i].parentNode){
-            hide[i].parentNode.removeChild(hide[i]);
-        }
-    }
+let remove = function () {
+    this.parentNode.remove();
+};
+for (let i = 0; i < n.length; i++) {
+    n[i].addEventListener('click', remove, false);
 }
 
